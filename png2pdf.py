5f4c5c03f9e395939242f167f6605631db4dfb32
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2023-04-28 16:21:18 +0900

    add png2pdf.py

--- png2pdf.py
+++ png2pdf.py
@@ -1,8 +1,35 @@
 import os
-import img2pdf
+import os.path
+import sys
 
-path = '../' + input('IB/')
-os.chdir(path)
-pngs = os.listdir()
-with open('main.pdf', 'wb') as f:
-    f.write(img2pdf.convert(pngs))
+from PIL import Image
+from PyPDF2 import PdfMerger
+
+
+def main():
+    path = sys.argv[1]
+    os.chdir(path)
+    png_list = sorted(
+        filter(
+            lambda f: os.path.isfile(f) and f.endswith('.png'), os.listdir()
+        )
+    )
+
+    merger = PdfMerger()
+    pdf_list = []
+    for png in png_list:
+        with Image.open(png) as image:
+            pdf = png.split('.')[0] + '.pdf'
+            image.convert('RGB').save(pdf)
+            merger.append(pdf)
+            pdf_list.append(pdf)
+
+    merger.write('output.pdf')
+    merger.close()
+
+    for pdf in pdf_list:
+        os.remove(pdf)
+
+
+if __name__ == '__main__':
+    main()
